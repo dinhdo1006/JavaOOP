0ce4cf95c2b085e1de98dedf22b85151d1a01dcc
dinhdo1006 <dinhdoo1012@gmail.com>
2024-01-31 23:55:50 +0700

    bai4

---
+++ lab2/src/bai3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class bai3 {
+    public static void main(String[] args) {
+        int tuoi , nam ;
+        String ten , nhomTuoi;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao ten:"); 
+        ten = sc.nextLine();
+         System.out.println("Nhap vao nam sinh:");
+        nam = sc.nextInt();
+        tuoi = 2024 - nam;
+        if( tuoi < 16){
+            nhomTuoi = " o do tuoi vi thanh nien.";
+            } else if (tuoi >= 16 || tuoi < 18) {
+                nhomTuoi = " tuoi truong thanh. " ;
+            } else{
+                nhomTuoi = " gia roi !";
+            }
+ System.out.println(" Ban " + ten + " thuoc  nhom  " +  nhomTuoi);
+        }
+
+        }
