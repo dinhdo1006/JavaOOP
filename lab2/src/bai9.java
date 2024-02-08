2f8f05292a145296b6330d5ed913780c81ac9869
dinhdo1006 <dinhdoo1012@gmail.com>
2024-02-08 20:54:33 +0700

    bai9

---
+++ lab2/src/bai9.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class bai9 {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        //Nhap xau chuoi ki tu
+        System.out.print("Nhap chuoi ki tu : ");
+        String chuoiKiTu = input.nextLine();
+
+        //Khoi tao gia tri
+        int countNumbers = 0 , countCap = 0 , countNomarl = 0;
+
+        //Vong lap
+        for(int i=0;i < chuoiKiTu.length();i++) {
+            if(chuoiKiTu.charAt(i) >= 97 && chuoiKiTu.charAt(i) <= 122 ) {
+                ++countNomarl;
+            } else if (chuoiKiTu.charAt(i) >= 65 && chuoiKiTu.charAt(i) <= 90) {
+                ++countCap;
+            } else if (chuoiKiTu.charAt(i) >= 48 && chuoiKiTu.charAt(i) <= 57) {
+                ++countNumbers;
+            } else {
+                System.out.println("Chuoi co ki tu khong xac dinh");
+            }
+        }
+    }
+}
